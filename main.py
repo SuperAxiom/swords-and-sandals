6a5f9e126e5652749330f4d9c7ff0592b94f40e5
SuperAxiom <superaxiom@proton.me>
2022-12-20 20:53:13 +0100

    Adding standart pyagame init script

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
-a = 5
-b = 6
-print(a + b)
+import os
+import pygame
+
+pygame.display.init()
+pygame.display.set_caption("Swords And Sandals")
+screen = pygame.display.set_mode((1280, 720))
+screen.fill(pygame.Color(255, 0, 0))
+pygame.display.flip()
+
+clock = pygame.time.Clock()
+
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    clock.tick(60)
+
+pygame.quit()
